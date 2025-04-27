ae41d8ad986096175f10fccafaee98dc05a148be
Badsha Sheih <badshasheikh361@gmail.com>
2025-04-27 21:21:30 +0600

    Add files via upload

---
+++ wlecome.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int t;
+    cin >> t; // Read the number of test cases
+    int x,y;
+    cin >> x >> y; // Read the values of x and y for each test case
+    string s;
+    cin >> s; // Read the string s for each test case
+    cout<<(t+x+y)<<" "<< s << endl;
+   
+    
+  
+
+    return 0;
+}
