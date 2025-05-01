5e072870cd25f934f40196b0c32ebd175ea791b6
Badsha Sheih <badshasheikh361@gmail.com>
2025-05-01 17:14:39 +0600

    Add files via upload

---
+++ string1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() 
+    // Read a large number as a string
+{
+    string number;
+    cin >> number; // Read the large number as a string
+    int c= number.length();
+    cout<<c<<endl;
+
+    return 0;
+}
