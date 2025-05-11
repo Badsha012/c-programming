0de957c61824b5ef4843a0eb5d3a898791a69375
Badsha Sheih <badshasheikh361@gmail.com>
2025-05-11 14:22:39 +0600

    Add files via upload

---
+++ 1practice.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,s;
+    printf("Enter two number: ");
+    scanf("%d %d", &a, &b);
+    s=a+b;
+    printf("sum of %d & %d is: %d", a,b,s);
+
+    return 0;
+
+}
