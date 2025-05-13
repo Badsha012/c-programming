e9f46b952e53df4ec48bbc2d9194527c95c57f94
Badsha Sheih <badshasheikh361@gmail.com>
2025-05-13 21:53:05 +0600

    Add files via upload

---
+++ cc+.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include "function.h"
+using namespace std;
+int fun (int x,int y,int z);
+
+int main()
+{
+
+    count << x "" << y << "" << z << endl;
+    vector<int> v;
+    v.push_back(x);
+    v.push_back(y);
+    v.push_back(z);
+    return x*x;
+
+}
